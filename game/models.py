399e000b8bef3579c2212e69c5b6026495e360d4
Tomima Ralijaona <tralijaona@emi.u-bordeaux1.fr>
2019-11-19 17:45:32 +0100

    creation of class Level & Player

--- game/models.py
+++ game/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Level(models.Model):
+    title = models.CharField(max_length = 255)
+    body = models.TextField() 
+
+    def __str__(self):
+        return self.title
+    
+class Player(models.Model):
+    login = models.CharField(max_length = 125)
+    nb_stars = models.IntegerField()            #min & max
+    time = models.TimeField()
+    id_level = models.ForeignKey('Level' , models.CASCADE)
