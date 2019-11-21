2626c248d72b8880759576d8389f99a9cf480376
julie_tilatti <julie.tilatti@etu.u-bordeaux.fr>
2019-11-21 19:46:25 +0100

    modification models.py (problems)

--- game/models.py
+++ game/models.py
@@ -1,14 +1,45 @@
 from django.db import models
 
 class Level(models.Model):
-    title = models.CharField(max_length = 255)
-    body = models.TextField()
+    name = models.CharField(max_length = 255)
+    image = models.TextField()
+    id_world = models.ForeignKey('World' , models.CASCADE)
 
     def __str__(self):
         return self.title
 
+
 class Player(models.Model):
-    login = models.CharField(max_length = 125)
+    login = models.CharField(max_length = 125, primary_key=True)
     nb_stars = models.IntegerField()            #min & max
     time = models.TimeField()
     id_level = models.ForeignKey('Level' , models.CASCADE)
+
+    def __str__(self):
+        return self.login
+
+
+class World(models.Model):
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Clue(models.Model):
+    description = models.TextField()
+    id_level = models.ForeignKey('Level' , models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Detail(models.Model):
+    image = models.TextField()
+    position_x = models.IntegerField()
+    position_y = models.IntegerField()
+    id_level = models.ForeignKey('Level' , models.CASCADE)
+
+    def __str__(self):
+        return self.image
