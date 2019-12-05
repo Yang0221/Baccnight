54567915f64158cfbf282351e2b018d2baff0806
Cherif <mcherifdiallo96@gmail.com>
2019-12-05 01:32:16 +0100

    Modification mystere

--- game/models.py
+++ game/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 
 class Level(models.Model):
-    title = models.CharField(max_length = 255)
-    body = models.TextField()
+    name = models.CharField(max_length = 255)
+    image = models.CharField(max_length = 255)
 
     def __str__(self):
-        return self.title
+        return self.name
 
 class Player(models.Model):
     login = models.CharField(max_length = 125)
     nb_stars = models.IntegerField()            #min & max
-    time = models.TimeField()
-    id_level = models.ForeignKey('Level' , models.CASCADE)
+    time = models.IntegerField()
+    id_level = models.ForeignKey('Level' ,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.login
+
+class Detail(models.Model):
+    image = models.CharField(max_length = 255)
+    position_x = models.IntegerField()
+    position_y = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+    id_level = models.ForeignKey('Level' ,on_delete=models.CASCADE)
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0m*$-b2*j19+c-4d^5f)sea6tspo9#eodp++xfm0s7m2^qoz*k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
