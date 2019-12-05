90878fc4f5e33b374468462fe3af8f66988e742d
Cherif <mcherifdiallo96@gmail.com>
2019-12-05 21:43:32 +0100

    dynamisation du site

--- game/views.py
+++ game/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Level
 from .models import World
+from .models import Detail
 from django.http import Http404
 
 def index(request):
@@ -9,9 +10,10 @@ def index(request):
 def show(request , id):
     try:
         level = Level.objects.get(id=id)
+        detail = Detail.objects.filter(id_level = id)
     except Level.DoesNotExist:
         raise Http404(id)
-    return render(request , 'game/level.html', { 'level' : level })
+    return render(request , 'game/level.html', { 'level' : level, 'detail' : detail})
 
 def form_user(request):
     return render(request , 'game/form_user.html')
